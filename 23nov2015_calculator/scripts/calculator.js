8b6181bd9260af368dc8fab29fc3131de2e02ad2
damianogiusti <damianogiusti95@gmail.com>
2015-11-24 10:53:14 +0100

    auto-push script by dg

--- 23nov2015_calculator/scripts/calculator.js
+++ 23nov2015_calculator/scripts/calculator.js
@@ -8,6 +8,11 @@ var current_num = '';
 var current_op = null;
 var flag = true;
 
+document.body.onload=(function() {
+    var altezza = document.documentElement.clientHeight;
+    //document.getElementById("calculator").style.height = altezza + "px";
+});
+
 function number(n) {
     // se non ho un operando
     // inserisco nella posizione 0
@@ -30,27 +35,35 @@ function operator(op) {
         // se ho gia messo un operatore
         // valuto l'espressione finora inserita
         // e mostro il risultato
-        if (current_op != null) {
+        if (current_op != null && numbers[1] != '') {
             evaluate_calc();
         }
+ 
+        if (current_op == null) {
+            display.innerHTML += " " + op;
 
-        // se non ho gia messo un operatore
-        /*else
-            numbers[0] = numbers[1];*/
-
-        //numbers[1] = '';
-
-        display.innerHTML += " " + op;
-
-        switch (op.trim()) {
-            case '+': current_op = function(a,b) { return a+b; };
-                break;
-            case '-': current_op = function(a,b) { return a-b; };
-                break;
-            case '*': current_op = function(a,b) { return a*b; };
-                break;
-            case '/': current_op = function(a,b) { return a/b; };
-                break;
+            switch (op.trim()) {
+                case '+':
+                    current_op = function (a, b) {
+                        return a + b;
+                    };
+                    break;
+                case '-':
+                    current_op = function (a, b) {
+                        return a - b;
+                    };
+                    break;
+                case '*':
+                    current_op = function (a, b) {
+                        return a * b;
+                    };
+                    break;
+                case '/':
+                    current_op = function (a, b) {
+                        return a / b;
+                    };
+                    break;
+            }
         }
     }
 }
@@ -63,15 +76,16 @@ function init() {
     numbers = ['',''];
     current_num = '';
     current_op = null;
+    flag = true;
     init_display();
 }
 
 function evaluate_calc() {
-    numbers[0] = current_op(parseInt(numbers[0], 10), parseInt(numbers[1], 10)).toString();
+    numbers[0] = current_op(parseFloat(numbers[0], 10), parseFloat(numbers[1], 10)).toString();
     numbers[1] = '';
     current_op = null;
     flag = false;
     init_display();
-
+    // console.log(numbers);
     display.innerHTML = numbers[0];
 }
